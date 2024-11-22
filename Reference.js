882215e81b1656ddb7b2329bbbbd17443aa3362a
suhailasrulsani-git <124322527+suhailasrulsani-git@users.noreply.github.com>
2024-11-22 22:09:10 +0800

    Create Reference.js

---
+++ Reference.js
@@ -0,0 +1,16 @@
+ Alert.innerHTML = `<div class="alert alert-danger floating-alert" role="alert">
+            Please insert value!
+        </div>`;
+
+        setTimeout(() => {
+            Alert.innerHTML = "";
+        }, 1000);
+        return
+
+const randomId = Math.floor(1000000000 + Math.random() * 9000000000);
+
+ListOfProjects.forEach((Item) => {
+})
+
+const projectObject = ListOfProjects.find(item => item.projectname === OptionCategoryEL.value);
+                
